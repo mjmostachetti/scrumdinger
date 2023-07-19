7fe3c1d9826ffe8deb52a73e704f8da82fb62387
Mike Mostachetti <mjmostachetti@gmail.com>
2023-07-19 06:40:04 -0700

    fun

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Michael Mostachetti on 7/2/23.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")){
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
