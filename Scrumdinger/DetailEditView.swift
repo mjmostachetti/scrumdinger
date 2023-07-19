ce3f7f5f206aed9d2f39285763346b060aae9617
Mike Mostachetti <mjmostachetti@gmail.com>
2023-07-19 07:20:12 -0700

    Adding data with bindings now

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -1,38 +1,50 @@
-//
-//  DetailEditView.swift
-//  Scrumdinger
-//
-//  Created by Michael Mostachetti on 7/2/23.
-//
-
 import SwiftUI
+
 
 struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
-            Section(header: Text("Meeting Info")){
+            Section(header: Text("Meeting Info")) {
                 TextField("Title", text: $scrum.title)
                 HStack {
                     Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
                     }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
                 }
             }
-            Section(header: Text("Attendees")){
+            Section(header: Text("Attendees")) {
                 ForEach(scrum.attendees) { attendee in
                     Text(attendee.name)
                 }
                 .onDelete { indices in
                     scrum.attendees.remove(atOffsets: indices)
                 }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
             }
         }
     }
 }
+
 
 struct DetailEditView_Previews: PreviewProvider {
     static var previews: some View {
