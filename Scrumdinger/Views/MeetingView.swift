a7790af6bef4c0feeae6df9deca455538e20b7fa
Mike Mostachetti <mjmostachetti@gmail.com>
2023-08-14 11:57:17 -0700

    adding StateObject to track scrums

---
+++ Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Michael Mostachetti on 6/26/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack(){
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}){
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
+
